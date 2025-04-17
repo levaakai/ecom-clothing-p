32fc89a4ef33ae08a7f31731078fe7d3db9ef180
levaakai <84318826+levaakai@users.noreply.github.com>
2025-04-17 13:11:47 +0000

    added postgress

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,11 +7,14 @@ import CartModel from '../models/Cart.js';
 import OrderModel from '../models/Order.js';
 import OrderItemModel from '../models/OrderItem.js';
 
+// console.log(process.env.DB_NAME);
+const sequelize = new Sequelize('postgresql://ecom_clothing_user:u4l6HJLZUiQPYH8XFzMaHkgCFpN05Iku@dpg-d00fhqpr0fns73e7t81g-a.virginia-postgres.render.com/ecom_clothing')
 
-const sequelize = new Sequelize('ecom_clothing', 'root', '2456', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres',
+//   port: process.env.DB_PORT || 5432 
+// });
 
 
 // 💡 Initialize models with sequelize + DataTypes
